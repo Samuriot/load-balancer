b7beb4a4371691188b3a074ff0fb6ce067419cf2
Samuriot <jomi.g.ruiz@gmail.com>
2026-09-01 20:26:26 -0700

    added IP parsing + adding to a list of IPS

--- main.go
+++ main.go
@@ -3,8 +3,50 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"regexp"
 )
 
+// from a high level overview, a load balancer is basically a router
+// it routes traffic across servers, depending on the algorithm chosen
+// round robin, weighted round robin, etc
+
+// components we need so far:
+// Array to store servers
+// counter to keep track of which server to send to next
+
+func check_ip_pattern(ip_addr string) bool {
+	re := regexp.MustCompile(`\b(?:\d{1,3}\.){3}\d{1,3}\b`)
+	if (re.MatchString(ip_addr)) {
+		return true;
+	}
+
+	return false;
+}
+
+func add_server(ip_addr string, ip_list *[]net.IP) bool {
+	if(ip_addr == "") {
+		return false;
+	}
+
+	if (!check_ip_pattern(ip_addr)) {
+		return false;
+	}
+
+	ip_obj := net.ParseIP(ip_addr);
+	*ip_list = append(*ip_list, ip_obj);
+	return true;
+}
+
 func main() {
-	fmt.Printf("hello world");
+	ip_list := []net.IP{};
+	ip_test := "192.168.1.1";
+	success := check_ip_pattern("192.168.1.1");
+	if (success) {
+		fmt.Printf("This is an IP\n");
+	}
+	add_server(ip_test, &ip_list);
+	for idx, ip := range ip_list {
+		fmt.Println(idx, ip);
+	}
 }
