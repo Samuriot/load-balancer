a4df7a32a6bb45ccf1430e7c6088a7cbfeb82377
Samuriot <jomi.g.ruiz@gmail.com>
2026-09-05 17:05:37 -0700

    committing whatever work I have currently

--- main.go
+++ main.go
@@ -15,6 +15,64 @@ import (
 // Array to store servers
 // counter to keep track of which server to send to next
 
+type LoadBalancer interface {
+	Init() bool
+	ProcessPacket() bool
+}
+
+type Poller interface {
+	Poll() ([byte], error)
+}
+
+type UDP_Poller struct {
+	conn *net.UDPConn
+}
+
+type L4_LoadBalancer struct {
+	backend_servers []net.IP
+}
+
+func (lb *L4_LoadBalancer) Init() bool {
+	tcp_sock, tcp_err := net.Listen("tcp", ":8080");
+	if tcp_err != nil {
+		fmt.Println(tcp_err);
+		return;
+	}
+
+	udp_sock, udp_err := net.Listen("udp", ":8080");
+	if udp_err != nil {
+		fmt.Println(err);
+		return;
+	}
+
+	go Poll(&tcp_sock);
+	go Poll(&udp_sock);
+}
+
+func (lb * L4_LoadBalancer) Poll() bool {
+	tcp_sock, tcp_err := net.Listen("tcp", ":8080");
+	if tcp_err != nil {
+		fmt.Println(tcp_err);
+		return;
+	}
+
+	udp_sock, udp_err := net.Listen("udp", ":8080");
+	if udp_err != nil {
+		fmt.Println(err);
+		return;
+	}
+
+	for { // golang doesn't have while true...
+		conn, err := sock.Accept();
+		if err != nil {
+			fmt.Println(err);
+			continue;
+		}
+
+		go ProcessPacket(conn);
+	}
+}
+
 func check_ip_pattern(ip_addr string) bool {
 	re := regexp.MustCompile(`\b(?:\d{1,3}\.){3}\d{1,3}\b`)
 	if (re.MatchString(ip_addr)) {
